790849c26a262127bdf35e1e075ef0dfb6ccc91b
Kalana Gunathilaka <105417887+kalanagunathilaka@users.noreply.github.com>
2023-11-04 01:14:30 +0530

    Create Backend "HappyDeals"

---
+++ Backend/happydeals/src/main/java/com/architects/happydeals/HappydealsApplication.java
@@ -0,0 +1,13 @@
+package com.architects.happydeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappydealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappydealsApplication.class, args);
+	}
+
+}
