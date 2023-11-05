ec3ef0246deff7cc0aab15744657ffbdfaffbe7f
RashiniNethmini <s.a.r.nethmini@gmail.com>
2023-11-05 16:36:02 +0530

    many-to-many_relaions_added

---
+++ Backend/happydeals/src/main/java/com/architects/happydeals/entity/OrderProduct.java
@@ -0,0 +1,32 @@
+package com.architects.happydeals.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderProduct  implements Serializable {
+
+    @EmbeddedId
+    private OrderProductId id;
+
+    @ManyToOne
+    @MapsId("productId")
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @MapsId("orderId")
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int quantityOrdered;
+
+    // Additional fields or methods can be added as needed
+}
