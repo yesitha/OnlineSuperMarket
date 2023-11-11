c1d5464a03ddb7f92c5b42d09bf3f41da52ffb24
RashiniNethmini <s.a.r.nethmini@gmail.com>
2023-11-11 20:28:49 +0530

    update Product

--- Backend/happyDealsBackendParent/InventoryService/src/main/java/com/architects/inventoryService/services/ProductService.java
+++ Backend/happyDealsBackendParent/InventoryService/src/main/java/com/architects/inventoryService/services/ProductService.java
@@ -1,56 +1,24 @@
 package com.architects.inventoryService.services;
 
+import com.architects.inventoryService.dto.request.RequestProductDto;
+import com.architects.inventoryService.dto.response.ResponseProductDto;
 import com.architects.inventoryService.entity.Product;
-import com.architects.inventoryService.Repositories.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
+
 
 import java.util.List;
 
-@Service
-public class ProductService {
 
-    @Autowired
-    private ProductRepository productRepository;
-    private final WebClient.Builder webClientBuilder;
-
-    public ProductService(WebClient.Builder webClientBuilder) {
-        this.webClientBuilder = webClientBuilder;
-    }
-
-    public void saveProduct(Product product) {
-        productRepository.save(product);
-    }
-
+public interface ProductService {
+    public void  createProduct(RequestProductDto product);
     // Update Product by productId
-    public void updateProduct(Long productId, Product updatedProduct) {
-        Product existingProduct = productRepository.findById(productId)
-                .orElseThrow(() -> new RuntimeException("Product not found with id " + productId));
-
-        existingProduct.setProductName(updatedProduct.getProductName());
-        existingProduct.setProductDescription(updatedProduct.getProductDescription());
-        existingProduct.setProductUnitPrice(updatedProduct.getProductUnitPrice());
-        existingProduct.setProductQuantityAvailable(updatedProduct.getProductQuantityAvailable());
-        existingProduct.setProductImage(updatedProduct.getProductImage());
-        existingProduct.setProductDiscount(updatedProduct.getProductDiscount());
-
-        productRepository.save(existingProduct);
-    }
+    public void updateProduct(Long productId, RequestProductDto updatedProduct);
 
     // Delete Product by productId
-    public void deleteProduct(Long productId) {
-        productRepository.deleteById(productId);
-    }
+    public void deleteProduct(Long productId);
 
     // Retrieve all Products
-    public List<Product> getAllProducts() {
-        return productRepository.findAll();
-    }
+    public List<ResponseProductDto> getAllProducts();
 
     // Retrieve Product by productId
-    public Product getProductById(Long productId) {
-        return productRepository.findById(productId)
-                .orElseThrow(() -> new RuntimeException("Product not found with id " + productId));
-    }
+    public Product getProductById(Long productId);
 }
