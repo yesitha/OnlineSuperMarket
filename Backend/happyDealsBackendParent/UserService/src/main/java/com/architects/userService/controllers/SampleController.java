4a1da615b005ce410a451ea0802e3ef591bcad96
Yesitha Sathsara <60166952+yesitha@users.noreply.github.com>
2023-11-11 23:11:47 +0530

    api gateway configurations added

--- Backend/happyDealsBackendParent/UserService/src/main/java/com/architects/userService/controllers/SampleController.java
+++ Backend/happyDealsBackendParent/UserService/src/main/java/com/architects/userService/controllers/SampleController.java
@@ -1,4 +1,12 @@
 package com.architects.userService.controllers;
 
+
+import jakarta.ws.rs.Path;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
 public class SampleController {
 }
