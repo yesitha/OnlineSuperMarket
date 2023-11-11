400bb5df389b89fdc1ec8107c98b7f98cbf082bb
NDJayasinghe <121921515+NDJayasinghe@users.noreply.github.com>
2023-11-11 06:18:52 +0530

    Initiating Cart Management

---
+++ Backend/happydeals/src/main/java/com/architects/happydeals/Repositories/CartRepository.java
@@ -0,0 +1,8 @@
+package com.architects.happydeals.Repositories;
+
+import com.architects.happydeals.entity.MainCart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository {
+
+}
