bc0e756d5ee19fb7cd7bb1706d62edeb2b2631d6
Yesitha Sathsara <60166952+yesitha@users.noreply.github.com>
2023-11-11 18:42:50 +0530

    Revert "Added UserService"
    This reverts commit ac9e2334d4735af2ae067cb7d805487a71cd4d96.

---
+++ Backend/happyDealsBackendParent/UserService/src/main/java/com/architects/userService/entity/Customer.java
@@ -0,0 +1,37 @@
+package com.architects.userService.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Customer {
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long customerId;
+
+    private String customerName;
+
+    @Embedded
+    private CustomerAddress customerAddress;
+
+    private String customerEmail;
+    private char[] customerPassword;
+    private String customerPhoneNumber;
+    private String designation;
+    private String userNotificationPreference;//SMS, EMAIL or  BOTH
+}
+
+@Embeddable
+class CustomerAddress {
+    private String street;
+    private String city;
+    private String district;
+}
--- Backend/happyDealsBackendParent/UserService/src/test/java/com/architects/userService/UserServiceApplicationTests.java
+++ Backend/happyDealsBackendParent/UserService/src/test/java/com/architects/userService/UserServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.supermarket.UserService;
+package com.architects.userService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UserServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
